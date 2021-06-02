ae330c9004cd92794bad4804a227a73c30a4809b
Nayeli I. Hernández Zárate <nayitze@gmail.com>
2021-06-02 18:34:06 -0500

    Añadí el estilo del encabezado al archivo style.css y el archivo productDetail.html con las partes básicas de la pagina

--- patines-cod/app.js
+++ patines-cod/app.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(express.static(__dirname + '/public')); 
 
+//app.get ('/', (req, res) => {
+  //   res.sendFile("/home.html", {root: "views"})
+//});
+
 app.get ('/', (req, res) => {
-     res.sendFile("/home.html", {root: "views"})
+     res.sendFile("/productDetail.html", {root: "views"})
 });
 
 const port = process.env.PORT || 3000
