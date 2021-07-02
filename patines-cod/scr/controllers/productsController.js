dd8b12f322a8a579183d93e1e869858966ac54fe
Nayeli I. Hernández Zárate <nayitze@gmail.com>
2021-07-01 19:13:39 -0500

    se añadio el formulario para eliminar y añadir modificar y crear productos

--- patines-cod/scr/controllers/productsController.js
+++ patines-cod/scr/controllers/productsController.js
@@ -2,6 +2,9 @@ const controlador = {
     detail: function (req, res) {
         res.render("productDetail");
     },
+    add: function(req, res){
+        res.render('productsAdd')
+    }
 };
 
 module.exports = controlador;
--- patines-cod/scr/routes/products.js
+++ patines-cod/scr/routes/products.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const productsController = require('../controllers/productsController.js');
 
 router.get('/detail', productsController.detail);
+router.get('/add', productsController.add);
 
 module.exports = router;
