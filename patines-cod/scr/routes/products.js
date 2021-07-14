5ef0ec2c1333bb64686fab37e53768d28f5bf92a
Leslie346 <lesliecruz346@gmail.com>
2021-07-13 20:34:10 -0500

    Avance de CRUD de index, detail y delete, se aÃcreo boton agregar producto a products-list, se agregaron rutas a products.js, carpeta modells

--- patines-cod/scr/routes/products.js
+++ patines-cod/scr/routes/products.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const productsController = require('../controllers/productsController.js');
 
+router.get('/products', productsController.index);
 router.get('/detail', productsController.detail);
+router.get('/detail/:id', productsController.detail);
 router.get('/create', productsController.form);
 router.post('/', productsController.crear)
+router.delete('/delete/:id', productsController.destroy);
 
 module.exports = router;
