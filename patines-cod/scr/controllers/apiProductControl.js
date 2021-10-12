0fc87e5b2af5ccaf0fb2225fc5bd4ce22c5109f4
Nayeli I. Hernández Zárate <nayitze@gmail.com>
2021-10-11 20:30:30 -0500

    Se hisi api productos

--- patines-cod/scr/controllers/apiProductControl.js
+++ patines-cod/scr/controllers/apiProductControl.js
@@ -3,36 +3,116 @@ const Op = db.Sequelize.Op;
 
 module.exports = {
     list: (req, res) => {
-        db.Productos.findAll({ raw: true }).then(function (products) {
+        db.Productos.findAll({ 
+            include: [{ association: "categorias" }]
+        }).then(function (products) {
             const productsList = []
             for (let i = 0; i < products.length; i++) {
                 productsList[i] = {
                     id: products[i].id,
                     name: products[i].name_product,
                     description: products[i].descripcion,
-                    detail: '/apiProducts/' + products[i].id
+                    category: products[i].categorias.name_category,
+                    detail: '/apiProducts/' + products[i].id,
+                    
                 }
-                db.Categorias.findAll({ 
-                    raw: true, 
-                    where: {
-                        id: products[i].id
-                }})
-                .then(function (categoria){
-                    productsList[i]={
-                        ...productsList[i],
-                        categoria: categoria
-                    }
-                })
             }
-            return res.status(200).json({
-                total: products.length,
-                data: productsList
-            }).catch(console.log())
+            db.Productos.findAll({
+                where:{
+                    category_id: 1, 
+                }
+            }).then( (patines) => {
+                db.Productos.findAll({
+                    where:{
+                        category_id: 2, 
+                    }
+                }).then( (proteccion) => {
+                    db.Productos.findAll({
+                        where:{
+                            category_id: 3, 
+                        }
+                    }).then((accesorios) => {
+                        db.Productos.findAll({
+                            where:{
+                                category_id: 4, 
+                            }
+                    }).then((refacciones) => {
+                        return res.status(200).json({
+                            total: products.length,
+                            countByCategory: {
+                                patines: patines.length,
+                                proteccion: proteccion.length,
+                                accesorios: accesorios.length,
+                                refacciones: refacciones.length
+                            },
+                            data: productsList
+                        })
+                    }).catch(console.log())
+                    })
+                })
+                
+            })
         })
     },
 
     detalle: (req, res) => {
-        const id = req.params.id
+        const idProduct = req.params.id;
+
+        db.Productos.findByPk(idProduct)
+            .then((productB) => {
+                db.Catalogo.findAll({
+                    where: {
+                        product_id: productB.id
+                    },
+                    include: [{ association: "colores" }]
+                })
+                    .then((catB) => {
+                        catB.map((uno) => {
+                            db.Existencias.findAll({
+                                where: {
+                                    product_catalogue_id: uno.id
+                                },
+                                include: [{ association: "tallas" }]
+                            })
+                                .then((exist) => {
+                                    db.Marcas.findByPk(productB.brand_id).then((marca)=>{
+                                        console.log(productB, catB, exist, marca);
+                                        const color = [];
+                                        const imagen = [];
+                                        const tallas = [];
+                                        for (let i = 0; i < catB.length; i++){
+                                            color[i]=catB[i].colores.color;
+                                            imagen[i]=catB[i].url_imagen;
+                                        }
+                                        for (let i = 0; i < catB.length; i++){
+                                            tallas[i]={
+                                                talla: exist[i].tallas.value_size,
+                                                existencia: exist[i].quantity
+                                            };
+                                        }
+                                    
+
+
+                                        return res.status(200).json({
+                                            name: productB.name_product,
+                                            price: productB.price,
+                                            marca: marca.name_brand,
+                                            descripcion: productB.descripcion,
+                                            categoria: catB.name_category,
+                                            color:color,
+                                            talla:tallas,
+                                            img_product: imagen
+                                        })
+                                    }).catch(e => console.log(e))
+                                    
+                                })
+                                
+                        })
+                    })
+
+            })
+
+       /* const id = req.params.id
 
         db.Productos.findByPk(id).then(productDetail => {
             return res.status(200).json({
@@ -40,7 +120,7 @@ module.exports = {
                 price: productDetail.price,
                 img_product: productDetail.img_product
             })
-        })
+        })*/
     }
 
 }
