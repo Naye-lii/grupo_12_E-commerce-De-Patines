5a56f25344c4ed92a8baa385fcf724c9481a3898
Nayeli I. Hernández Zárate <nayitze@gmail.com>
2021-10-13 21:23:29 -0500

    Se separaron proyectos

--- patines-react/src/components/Titulo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function Titulo (){
-    return(
-        <div className = "App-header">
-          <h1>Dashboard</h1>  
-        </div>
-    )
-}
-export default Titulo;
